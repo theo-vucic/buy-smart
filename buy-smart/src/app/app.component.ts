bbb65376b13624edd347fcaa16c88334d4deec4c
Lin <celine.jiang12@gmail.com>
2024-03-04 12:09:19 +0100

    modify header

--- buy-smart/src/app/app.component.ts
+++ buy-smart/src/app/app.component.ts
@@ -1,13 +1,55 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+
+import {
+  FormBuilder,
+  FormControl,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+
+import {
+  FloatLabelType,
+  MatFormFieldModule,
+} from '@angular/material/form-field';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    CommonModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule,
+    MatInputModule,
+    MatIconModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'buy-smart';
+  searchQuery: string = ''; // Property to bind with the input field
+  hide = true;
+
+  hideRequiredControl = new FormControl(false);
+  floatLabelControl = new FormControl('auto' as FloatLabelType);
+
+  handleSearchInput(inputField: HTMLInputElement) {
+    if (!inputField.value) {
+      inputField.placeholder = 'Chercher sur Buy Smart';
+    } else {
+      inputField.placeholder = '';
+    }
+  }
+
+  search(inputField: HTMLInputElement) {}
 }
--- buy-smart/src/app/page-component/page-component.component.ts
+++ buy-smart/src/app/page-component/page-component.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-page-component',
